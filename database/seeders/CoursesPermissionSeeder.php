ea0f8e7a8845354d2ca87213ca12f58acf595eb7
Mateusz Qunabu <mateusz@qunabu.com>
2021-05-08 14:41:47 +0200

    seeder & permissions fix (#31)
    * seeder & permissions fix
    
    * rm openapi.json

--- database/seeders/CoursesPermissionSeeder.php
+++ database/seeders/CoursesPermissionSeeder.php
@@ -12,7 +12,7 @@ class CoursesPermissionSeeder extends Seeder
     public function run()
     {
         // create permissions
-        Role::findOrCreate('admin', 'api');
+        $admin = Role::findOrCreate('admin', 'api');
         $tutor = Role::findOrCreate('tutor', 'api');
 
         Permission::findOrCreate('update course', 'api');
@@ -20,9 +20,7 @@ class CoursesPermissionSeeder extends Seeder
         Permission::findOrCreate('create course', 'api');
         Permission::findOrCreate('attend course', 'api');
 
-        $tutor->givePermissionTo('update course');
-        $tutor->givePermissionTo('delete course');
-        $tutor->givePermissionTo('create course');
-        $tutor->givePermissionTo('attend course');
+        $admin->givePermissionTo(['update course', 'delete course', 'create course', 'attend course']);
+        $tutor->givePermissionTo(['update course', 'delete course', 'create course', 'attend course']);
     }
 }
