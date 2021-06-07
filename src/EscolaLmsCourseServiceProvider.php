236bde82d9be4907a9aa91e7e29f45879dfcb6cc
Mateusz <mateusz@qunabu.com>
2021-06-07 13:47:05 +0200

    AuthServiceProvider in composer.json providers

--- src/EscolaLmsCourseServiceProvider.php
+++ src/EscolaLmsCourseServiceProvider.php
@@ -8,7 +8,9 @@ use EscolaLms\Courses\Services\Contracts\CourseServiceContract;
 use EscolaLms\Courses\Services\Contracts\ProgressServiceContract;
 use EscolaLms\Courses\Services\CourseService;
 use EscolaLms\Courses\Services\ProgressService;
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+//use Illuminate\Support\ServiceProvider;
 use EscolaLms\Courses\Repositories;
 use EscolaLms\Courses\Models\TopicContent\Audio;
 use EscolaLms\Courses\Models\TopicContent\Video;
@@ -16,6 +18,7 @@ use EscolaLms\Courses\Models\TopicContent\Image;
 use EscolaLms\Courses\Models\TopicContent\RichText;
 use EscolaLms\Courses\Models\TopicContent\H5P;
 use EscolaLms\Courses\Repositories\TopicRepository;
+use EscolaLms\Courses\AuthServiceProvider;
 
 class EscolaLmsCourseServiceProvider extends ServiceProvider
 {
@@ -25,14 +28,20 @@ class EscolaLmsCourseServiceProvider extends ServiceProvider
         CourseRepositoryContract::class => CourseRepository::class
     ];
 
+    protected $policies = [
+        Course::class => CoursesPolicy::class,
+    ];
+
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__ . '/routes.php');
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        $this->registerPolicies();
     }
 
     public function register()
     {
+        $this->app->register(AuthServiceProvider::class);
         TopicRepository::registerContentClass(Audio::class);
         TopicRepository::registerContentClass(Video::class);
         TopicRepository::registerContentClass(Image::class);
