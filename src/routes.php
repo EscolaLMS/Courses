fa80cdf14ee8e8082b429299475454132f9c21b5
Mateusz Qunabu <mateusz@qunabu.com>
2021-06-29 16:44:32 +0200

    Update routes.php
    changing order of the routes to avoid collsion `/courses/progress` vs `/course/:id`

--- src/routes.php
+++ src/routes.php
@@ -8,6 +8,15 @@ use EscolaLms\Courses\Http\Controllers\TopicRichTextAPIController;
 use Illuminate\Routing\ImplicitRouteBinding;
 
 Route::group(['middleware' => ['api'], 'prefix' => 'api'], function () {
+    Route::group(['prefix' => '/courses/progress'], function () {
+        Route::get('/', [CourseProgressAPIController::class, 'index']);
+        Route::get('/{course_id}', [CourseProgressAPIController::class, 'show']);
+        Route::patch('/{course_id}', [CourseProgressAPIController::class, 'store']);
+
+        Route::put('/{topic_id}/ping', [CourseProgressAPIController::class, 'ping']);
+        Route::post('/{topic_id}/h5p', [CourseProgressAPIController::class, 'h5p']);
+    });
+
     Route::get('courses/{course}/program', [CourseAPIController::class, 'program']);
     Route::post('courses/sort', [CourseAPIController::class, "sort"]);
     Route::post('courses/{course}', [CourseAPIController::class, 'update']);
@@ -17,21 +26,11 @@ Route::group(['middleware' => ['api'], 'prefix' => 'api'], function () {
     Route::resource('topics', TopicAPIController::class);
     Route::post('topics/{topic}', [TopicAPIController::class, "update"]);
 
-    
+
     Route::get('/courses/search/tags', [CourseAPIController::class, 'searchByTag']);
     Route::get('/courses/search/{category_id}', [CourseAPIController::class, 'category']);
     Route::group(['prefix' => '/courses/attach/{id}/'], function () {
         Route::post('categories', [CourseAPIController::class, 'attachCategory']);
         Route::post('tags', [CourseAPIController::class, 'attachTags']);
-    });
-
-
-    Route::group(['prefix' => '/progress'], function () {
-        Route::get('/', [CourseProgressAPIController::class, 'index']);
-        Route::get('/{course_id}', [CourseProgressAPIController::class, 'show']);
-        Route::patch('/{course_id}', [CourseProgressAPIController::class, 'store']);
-
-        Route::put('/{topic_id}/ping', [CourseProgressAPIController::class, 'ping']);
-        Route::post('/{topic_id}/h5p', [CourseProgressAPIController::class, 'h5p']);
     });
 });
