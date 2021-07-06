4bd33b5441c0a194ebbebf7461cdfd8fa03cb7f1
Mateusz <mateusz@qunabu.com>
2021-07-06 16:14:32 +0200

    add author to response

--- tests/Models/User.php
+++ tests/Models/User.php
@@ -6,9 +6,17 @@ use EscolaLms\Core\Models\User as UserCore;
 use EscolaLms\Courses\Models\Traits\HasCourses;
 use EscolaLms\Courses\Tests\Database\Factories\UserFactory;
 
+use EscolaLms\Auth\Models\Traits\HasOnboardingStatus;
+use EscolaLms\Auth\Models\Traits\UserHasSettings;
+
 class User extends UserCore
 {
-    use HasCourses;
+    use HasCourses, HasOnboardingStatus, UserHasSettings;
+
+    protected function getTraitOwner(): self
+    {
+        return $this;
+    }
 
     public static function newFactory()
     {
