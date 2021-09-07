7685a31669c7c78066575472e9a6e42329032ae7
Paweł Chołaj <pawel.cholaj@escola.pl>
2021-09-07 13:45:16 +0200

    Moved progress seeder to courses package

---
+++ database/seeders/ProgressSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace EscolaLms\Courses\Database\Seeders;
+
+use EscolaLms\Core\Enums\UserRole;
+use EscolaLms\Courses\Enum\ProgressStatus;
+use EscolaLms\Courses\Models\Course;
+use EscolaLms\Courses\Repositories\CourseProgressRepository;
+use EscolaLms\Courses\Services\ProgressService;
+use EscolaLms\Courses\Tests\Models\User;
+use Illuminate\Database\Seeder;
+
+class ProgressSeeder extends Seeder
+{
+    public function run()
+    {
+        $students = User::role(UserRole::STUDENT)->whereHas('courses')->take(10)->get();
+        if (empty($students)) {
+            User::role(UserRole::STUDENT)->take(10)->get();
+            foreach ($students as $student) {
+                $student->courses()->save(Course::inRandomOrder()->first());
+            }
+        }
+        $progressService = app(ProgressService::class);
+        $progressRepository = app(CourseProgressRepository::class);
+        foreach ($students as $student) {
+            $progressedCourses = $progressService->getByUser($student);
+            foreach ($progressedCourses as $course) {
+                /** @var Course $course */
+                foreach ($course->topic as $topic) {
+                    $status = ProgressStatus::getRandomValue();
+                    $progressRepository->updateInTopic($topic, $student, $status, $status !== ProgressStatus::INCOMPLETE ? rand(60, 300) : null);
+                    if ($status === ProgressStatus::IN_PROGRESS) {
+                        $progressService->ping($student, $topic);
+                    }
+                }
+                $progressService->update($course, $student, []);
+            }
+        }
+    }
+}
