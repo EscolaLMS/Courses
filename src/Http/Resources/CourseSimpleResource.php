6c3992d862d5eb2327187cfa1769d1d8a52400ca
Paweł Chołaj <pawel.cholaj@escolasoft.com>
2021-11-08 09:31:15 +0100

    Test deleting topic by deleting whole course

--- src/Http/Resources/CourseSimpleResource.php
+++ src/Http/Resources/CourseSimpleResource.php
@@ -2,8 +2,16 @@
 
 namespace EscolaLms\Courses\Http\Resources;
 
+use EscolaLms\Auth\Traits\ResourceExtandable;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class CourseSimpleResource extends JsonResource
 {
+    use ResourceExtandable;
+
+    public function toArray($request)
+    {
+        $fields = parent::toArray($request);
+        return self::apply($fields, $this);
+    }
 }
