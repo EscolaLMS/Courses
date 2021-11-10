d3404e30771abdb6c9298881cdceccafb7a62902
Mateusz <mateusz@qunabu.com>
2021-11-10 13:03:48 +0100

    remove unsesarry try/catch block

--- database/seeders/CoursesSeeder.php
+++ database/seeders/CoursesSeeder.php
@@ -5,7 +5,7 @@ namespace EscolaLms\Courses\Database\Seeders;
 use EscolaLms\TopicTypes\Database\Seeders\CoursesWithTopicSeeder;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class CoursesSeeder extends Seeder
 {
     /**
      * Run the database seeds.
