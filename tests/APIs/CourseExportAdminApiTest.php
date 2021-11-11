ae753fe01e393ecab4756217869262474e51f61c
Mateusz <mateusz@qunabu.com>
2021-11-11 17:32:08 +0100

    exporter base

--- tests/APIs/CourseExportAdminApiTest.php
+++ tests/APIs/CourseExportAdminApiTest.php
@@ -26,6 +26,53 @@ class CourseExportAdminApiTest extends TestCase
         $this->user->assignRole('admin');
 
         Storage::fake(config('filesystems.default'));
+
+        Storage::fake(config('filesystems.default'));
+        Storage::put('dummy.mp4', 'Some dummy data');
+        Storage::put('dummy.mp3', 'Some dummy data');
+        Storage::put('dummy.jpg', 'Some dummy data');
+        Storage::put('dummy.png', 'Some dummy data');
+        Storage::put('dummy.pdf', 'Some dummy data');
+
+        $course = Course::factory()->create([
+            'author_id' => $this->user->id,
+        ]);
+        $lesson = Lesson::factory()->create([
+            'course_id' => $course->id,
+        ]);
+        $topic_audio = Topic::factory()->create([
+            'lesson_id' => $lesson->id,
+        ]);
+        $topic_image = Topic::factory()->create([
+            'lesson_id' => $lesson->id,
+        ]);
+        $topic_pdf = Topic::factory()->create([
+            'lesson_id' => $lesson->id,
+        ]);
+        $topic_video = Topic::factory()->create([
+            'lesson_id' => $lesson->id,
+        ]);
+
+        $topicable_audio = Audio::factory()->create([
+            'value' => 'dummy.mp3',
+        ]);
+        $topicable_image = Image::factory()->create([
+            'value' => 'dummy.jpg',
+        ]);
+        $topicable_pdf = PDF::factory()->create([
+            'value' => 'dummy.pdf',
+        ]);
+        $topicable_video = Video::factory()->create([
+            'value' => 'dummy.mp4',
+            'poster' => 'dummy.png',
+        ]);
+
+        $topic_audio->topicable()->associate($topicable_audio)->save();
+        $topic_image->topicable()->associate($topicable_image)->save();
+        $topic_pdf->topicable()->associate($topicable_pdf)->save();
+        $topic_video->topicable()->associate($topicable_video)->save();
+
+        $this->course = $course;
     }
 
     /**
@@ -33,11 +80,11 @@ class CourseExportAdminApiTest extends TestCase
      */
     public function testExportiCreated()
     {
-        $course = Course::factory()->create();
+        $id = $this->course->id;
 
         $this->response = $this->actingAs($this->user, 'api')->json(
             'GET',
-            '/api/admin/courses/'.$course->id.'/export/'
+            '/api/admin/courses/'.$id.'/export/'
         );
 
         $this->response->assertOk();
@@ -46,7 +93,7 @@ class CourseExportAdminApiTest extends TestCase
 
         $filename = basename($data->data);
 
-        $filepath = sprintf('exports/courses/%d/%s', $course->id, $filename);
+        $filepath = sprintf('exports/courses/%d/%s', $id, $filename);
 
         Storage::assertExists($filepath);
     }
