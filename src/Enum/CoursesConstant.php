0707bcfe83ddd94e6757df124addd296599360c6
Hubert Krzysztofiak <62691459+HerbertIV@users.noreply.github.com>
2021-12-22 16:11:06 +0100

    Issue/#91 (#178)
    * Add new event escolaLms and remove old event
    
    * Add dispatches events to code - work in progress
    
    * Add tests to courses for events
    
    * Add dev-main to topic-types
    
    * Add deadline reminder term to config
    
    * Update composer.json
    
    Co-authored-by: Mateusz Qunabu <mateusz@qunabu.com>

---
+++ src/Enum/CoursesConstant.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace EscolaLms\Courses\Enum;
+
+use EscolaLms\Core\Enums\BasicEnum;
+
+class CoursesConstant extends BasicEnum
+{
+    // constant is expressed in days
+    public const REMINDER_OF_DEADLINE_COUNT_DAYS = 7;
+}
