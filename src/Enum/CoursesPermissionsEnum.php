09c5fa49311acf2d9a2d0733bf20bae61d3c10a9
KD <do>
2022-01-21 12:22:31 +0100

    #94 - fix

--- src/Enum/CoursesPermissionsEnum.php
+++ src/Enum/CoursesPermissionsEnum.php
@@ -10,10 +10,10 @@ class CoursesPermissionsEnum extends BasicEnum
     const COURSE_CREATE = 'course_create';
     const COURSE_UPDATE = 'course_update';
     const COURSE_DELETE = 'course_delete';
-    const COURSE_ATTEND = 'course_attend';
+    const COURSE_ATTEND = 'course_read';
     const COURSE_UPDATE_OWNED = 'course_update_authored';
     const COURSE_DELETE_OWNED = 'course_delete_authored';
-    const COURSE_ATTEND_OWNED = 'course_attend_authored';
+    const COURSE_ATTEND_OWNED = 'course_read_authored';
 
     const LESSON_UPDATE = 'update';
     const LESSON_ATTEND = 'attend';
