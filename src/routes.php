b63ff85fc125324cb2c40d3d4e31d36aacd9aa89
Hubert Krzysztofiak <hubert.krzysztofiak@escolasoft.com>
2022-05-05 20:04:39 +0200

    Remove custom Route

--- src/routes.php
+++ src/routes.php
@@ -7,7 +7,7 @@ use EscolaLms\Courses\Http\Controllers\CourseProgressAPIController;
 use EscolaLms\Courses\Http\Controllers\LessonAPIController;
 use EscolaLms\Courses\Http\Controllers\TopicAPIController;
 use EscolaLms\Courses\Http\Controllers\TopicResourcesAPIController;
-use EscolaLms\Core\Http\Facades\Route;
+use Illuminate\Routing\Route;
 
 // admin endpoints
 Route::group(['middleware' => ['auth:api'], 'prefix' => 'api/admin'], function () {
