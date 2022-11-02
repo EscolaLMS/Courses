bf10ddf9fd916b156d21db3767d80e8df99ca9ac
Mateusz Wojczal <mateusz@wojczal.com>
2022-11-02 14:55:37 +0100

    public attribute

--- database/migrations/2022_11_02_133941_add_column_public.php
+++ database/migrations/2022_11_02_133941_add_column_public.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class DropBasePriceAndPurchasableFromCoursesTable extends Migration
+class AddColumnPublic extends Migration
 {
     public function up()
     {
